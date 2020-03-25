8168ee7b17515db1fc352907961e39239c66ef12
Ryan Budhu <ryan.j.budhu@gmail.com>
2020-03-25 16:30:58 -0400

    More progess on question 4

--- ThankUVertext/TopSort.py
+++ ThankUVertext/TopSort.py
@@ -2,8 +2,40 @@ class TopSort:
 	def __init__(self):
 		pass
 	
-	def Kahns(graph):
-		pass
+	def Kahns(self, graph):
+		nodes = graph.getAllNodes()
+		in_degree = dict(((i.name,0) for i in nodes))
+		for i in nodes:
+			for n in i.neighbors:
+				in_degree[n.name] += 1
+		queue = []
+		queue = list(filter(lambda i: in_degree[i.name]==0, nodes))
+		output = []
+		print(in_degree)
+		while queue:
+			print([i.name for i in queue])
+			print(in_degree)
+			cur = queue.pop(0)
+			output.append(cur)
+			for i in cur.neighbors:
+				in_degree[i.name] -= 1
+				if in_degree[i.name] == 0:
+					queue.append(i)
+		return output
 	
-	def mDFS(graph):
-		pass
+	def mDFS(self, graph):
+		for i in graph.getAllNodes():
+			path = self.DFS(i)
+			'''if path:
+				print([i.name for i in path])
+			else:
+				print(path)'''
+		return path
+	
+	def DFS(self, cur, path=[]):
+		if cur in path:
+			return path
+		path.append(cur)
+		for i in cur.neighbors:
+			if i not in path:
+				return self.DFS(i,path)
--- ThankUVertext/main.py
+++ ThankUVertext/main.py
@@ -1,6 +1,35 @@
 from DirectedGraph import DirectedGraph
-from ../TraverseThisVertext/Label import createLabel
+from TopSort import TopSort
+from Label import createLabel
+from random import sample,randrange,choice
 
 def createRandomDAGIter(n):
-	pass
+	g = DirectedGraph()
+	for i in range(1,n+1):
+		g.addNode(createLabel(i))
+	nodes = g.getAllNodes()
+	weights = list(range(0,len(nodes),10))
+	order =[]
+	for i in nodes:
+		neighs = randrange(1, n//2)
+		x = nodes.index(i)
+#		suggested = choices(nodes[:x]+nodes[x+1:],weights=weights[:x]+weights[x+1:],k=neighs)
+		suggested = sample(nodes[:x]+nodes[x+1:],k=neighs)
+		for j in suggested:
+			if j == nodes[0]:
+				g.addDirectedEdge(i, choice(nodes[1:nodes.index(i)]+nodes[nodes.index(i)+1:]))
+				continue
+			g.addDirectedEdge(i, j)
+#	choice(nodes).neighbors = []
+	return g
 
+
+def test():
+	g = createRandomDAGIter(5)
+	ts = TopSort()
+	print([{i.name:[j.name for j in i.neighbors]} for i in g.getAllNodes()])
+	kahnsPath = ts.Kahns(g)
+#	mDFSPath = ts.mDFS(g)
+	print([i.name for i in kahnsPath])
+#	print([i.name for i in mDFSPath])
+test()

7e7b2e512ef68f5d43cddd73d8c86aa02818ca83
Ryan Budhu <ryan.j.budhu@gmail.com>
2020-03-26 11:58:55 -0400

    added comments

--- ThankUVertext/main.py
+++ ThankUVertext/main.py
@@ -1,21 +1,35 @@
 from DirectedGraph import DirectedGraph
 from TopSort import TopSort
 from Label import createLabel
-from random import sample,randrange,choice,shuffle
+from random import sample,randrange,shuffle
 
 def createRandomDAGIter(n):
+	# Create new DirectedGraph and add n amount of nodes to it
 	g = DirectedGraph()
 	for i in range(1,n+1):
 		g.addNode(createLabel(i))
+		
+	# Copy the list of the nodes from the graph
+	# so we can pop from list
 	nodes = g.getAllNodes().copy()
+	
+	# Shuffle the nodes so the graph doesn't
+	# start with "A" every time
 	shuffle(nodes)
-	total = len(nodes)
+	# While there are nodes in the list
 	while len(nodes) > 0:
 		cur = nodes.pop(0)
 		if len(nodes)<=1:
 			break
+			
+		# Choose a random amount of children
 		num = randrange(1,len(nodes))
+		
+		# Add a random sample of num length
+		# the neighbor of the cur
 		for i in sample(nodes,num): g.addDirectedEdge(cur, i)
+		
+		# For every neighbor of cur do the same
 		for n in cur.neighbors:
 			nodes.pop(nodes.index(n))
 			if len(nodes) <= 1:

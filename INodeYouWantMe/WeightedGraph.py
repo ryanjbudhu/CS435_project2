9fd09f1e91110428dda268ce3aec2519a6b54d03
Ryan Budhu <ryan.j.budhu@gmail.com>
2020-03-27 11:35:50 -0400

    Started question 5

---
+++ INodeYouWantMe/WeightedGraph.py
@@ -0,0 +1,27 @@
+class WeightedGraph:
+	class Node:
+		def __init__(self, name):
+			self.name = name
+			self.neighbors = {}
+	def __init__(self):
+		self.nodes = []
+	
+	def addNode(self, nodeName):
+		self.nodes.append(self.Node(nodeName))
+	
+	#This adds an undirected weighted edge between first and second (but not vice versa)
+	def addDirectedEdge(self, first, second, weight):
+		if first in second.neighbors:
+			return
+		first.neighbors[second] = weight
+	
+	#This removes an undirected weighted edge between first and second (but not vice versa)
+	def removeDirectedEdge(self, first, second):
+		try:
+			del first.neighbors[second]
+		except:
+			return
+	
+	#This returns all the nodes in the graph
+	def getAllNodes(self):
+		return self.nodes

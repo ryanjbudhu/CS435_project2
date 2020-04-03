520637c6e07a0e6d0913aad1f53b047fc2a10f3c
Ryan Budhu <ryan.j.budhu@gmail.com>
2020-04-03 10:26:20 -0400

    finished a* algorithm

--- WishUponA/main.py
+++ WishUponA/main.py
@@ -17,26 +17,30 @@ def createRandomGridGraph(n):
 		g.addGridNode(x, y, createLabel(idx)) # Insert a node at (x,y)
 		nodes = g.getAllNodes() # Get the updated node grid
 		if colcount > 0: # If its not the leftmost node in grid
-			if randint(0, 1):
+			if randint(0, 2):
 				g.addUndirectedEdge(nodes[x][y-1], nodes[x][y])
 		if idx > cols: # If its not the first row
-			if randint(0, 1):
+			if randint(0, 2):
 				g.addUndirectedEdge(nodes[x-1][y], nodes[x][y])
 		colcount += 1
 		idx += 1
 	return g
 
 
-def astar(start):
+def astar(start, end):
 	openList = {start:0}
 	closedList = []
 	
-	while openList:
+	while openList and end not in closedList:
 		q = min(openList, key=lambda x: openList[x])
 		cur = openList[q]
 		del openList[q]
-		print(q,':',cur)
+		for n in q.neighbors:
+			if (n not in closedList):
+				openList[n] = abs(n.x - end.x) + abs(n.y - end.y)
 		closedList.append(q)
+	if end not in closedList:
+		return []
 	return closedList
 
 
@@ -58,12 +62,7 @@ def testGrid(showN=False):
 	sourceNode = nodes[0][0]
 	destNode = nodes[n-1][n-1]
 	printGrid(nodes,showN)
+	order = astar(sourceNode, destNode)
+	print([i.name for i in order])
+	
 testGrid(False)
-
-'''
-A	B	C
-D	E	F
-G	H	I
-J	K	L
-M	N
-'''

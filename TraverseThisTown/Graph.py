b1f290265e9c4a76d7db0cfee6c19c63c3570127
Ryan Budhu <ryan.j.budhu@gmail.com>
2020-04-10 09:51:49 -0400

    changed neighbor list into set

--- TraverseThisTown/Graph.py
+++ TraverseThisTown/Graph.py
@@ -2,7 +2,7 @@ class Graph:
 	class Node:
 		def __init__(self, name):
 			self.name = name
-			self.neighbors = []
+			self.neighbors = set()
 	def __init__(self):
 		self.nodes = []
 	
@@ -11,18 +11,13 @@ class Graph:
 	
 	#This adds an undirected edge between first and second (and vice versa)
 	def addUndirectedEdge(self, first, second):
-		if first in second.neighbors or second in first.neighbors:
-			return
-		first.neighbors.append(second)
-		second.neighbors.append(first)
+		first.neighbors.add(second)
+		second.neighbors.add(first)
 	
 	#This removes an undirected edge between first and second (and vice versa)
 	def removeUndirectedEdge(self, first, second):
-		try:
-			first.neighbors.remove(second)
-			second.neighbors.remove(first)
-		except:
-			return
+		first.neighbors.discard(second)
+		second.neighbors.discard(first)
 	
 	#This returns all the nodes in the graph
 	def getAllNodes(self):
